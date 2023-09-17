7fb8f783e6baa538a8491743fce52b0b74b81faa
Paul Mullins <personinternet@protonmail.com>
2023-09-17 09:30:19 -0400

    Tried treating discord_id's as primary keys best I could, does not work for authors because of guild nicknames, should probably use guild in the ID of the author when indexing

--- fix_schema.py
+++ fix_schema.py
@@ -66,13 +66,61 @@ FROM raw_attachments_t;
 """]
 
 table_type_queries = [
+
+"""
+CREATE TABLE IF NOT EXISTS guilds_t (
+  id          INTEGER PRIMARY KEY,
+  guild_id    TEXT,
+  guild_name  TEXT,
+  iconUrl     TEXT
+)
+""",
+
+"""
+INSERT INTO guilds_t (id, guild_name, iconUrl)
+SELECT
+  DISTINCT(json_extract(raw_json, '$.id')) as id,
+  json_extract(raw_json, '$.name') as guild_name,
+  json_extract(raw_json, '$.iconUrl') as iconUrl
+FROM raw_guilds_t;
+""",
+
+"""
+CREATE TABLE IF NOT EXISTS channels_t (
+  id            INTEGER PRIMARY KEY,
+  channel_name  TEXT,
+  channel_type  TEXT,
+  categoryId    TEXT,
+  category      TEXT,
+  guild_id      INTEGER,
+  topic         TEXT,
+  channel_name_length INT,
+  FOREIGN KEY (guild_id) REFERENCES guilds_t(id)
+)
+""",
+
+
+"""
+INSERT INTO channels_t (id, channel_name, channel_type, categoryId, category, guild_id, topic, channel_name_length)
+SELECT
+  DISTINCT( json_extract(raw_json, '$.id')) as id,
+  json_extract(raw_json, '$.name') as channel_name,
+  json_extract(raw_json, '$.type') as channel_type,
+  json_extract(raw_json, '$.categoryId') as categoryId,
+  json_extract(raw_json, '$.category') as category,
+  json_extract(raw_json, '$.guild_id') as guild_id,
+  json_extract(raw_json, '$.topic') as topic,
+  LENGTH(json_extract(raw_json, '$.name')) as channel_name_length
+FROM
+  raw_channels_t;
+""",
+
 """
 CREATE TABLE IF NOT EXISTS messages_t (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  message_id   TEXT,
+  id           INTEGER PRIMARY KEY,
   attachments  TEXT,
   author       TEXT,
-  channel_id   TEXT,
+  channel_id   INTEGER,
   content      TEXT,
   interaction  TEXT,
   isBot        BOOLEAN,
@@ -81,14 +129,42 @@ CREATE TABLE IF NOT EXISTS messages_t (
   msg_type     TEXT,
   timestamp    INT,
   timestampEdited INT,
-  content_length INT
+  content_length INT,
+  FOREIGN KEY (channel_id) REFERENCES channels_t(id)
 )
 """,
 
+
 """
-INSERT INTO messages_t (message_id, attachments, author, channel_id, content, interaction, isBot, isPinned, mentions, msg_type, timestamp, timestampEdited, content_length)
+CREATE TABLE IF NOT EXISTS authors_t (
+  id            INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id     INTEGER,
+  name          TEXT,
+  nickname      TEXT,
+  color         TEXT,
+  isBot         BOOLEAN,
+  avatarUrl     TEXT
+)
+""",
+
+
+"""
+INSERT INTO authors_t (author_id, name, nickname, color, isBot, avatarUrl)
 SELECT
-  json_extract(raw_json, '$.id') as message_id,
+  DISTINCT(json_extract(raw_json, '$.id')) as author_id,
+  json_extract(raw_json, '$.name') as name,
+  json_extract(raw_json, '$.nickname') as nickname,
+  json_extract(raw_json, '$.color') as color,
+  json_extract(raw_json, '$.isBot') as isBot,
+  json_extract(raw_json, '$.avatarUrl') as avatarUrl
+FROM raw_authors_t;
+""",
+
+
+"""
+INSERT INTO messages_t (id, attachments, author, channel_id, content, interaction, isBot, isPinned, mentions, msg_type, timestamp, timestampEdited, content_length)
+SELECT
+  json_extract(raw_json, '$.id') as id,
   json_extract(raw_json, '$.attachments') as attachments,
   json_extract(raw_json, '$.author') as author,
   json_extract(raw_json, '$.channel_id') as channel_id,
@@ -106,68 +182,19 @@ FROM
 """,
 
 """
-CREATE TABLE IF NOT EXISTS channels_t (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  channel_id    TEXT,
-  channel_name  TEXT,
-  channel_type  TEXT,
-  categoryId    TEXT,
-  category      TEXT,
-  guild_id      TEXT,
-  topic         TEXT,
-  channel_name_length INT
-)
-""",
-
-
-"""
-INSERT INTO channels_t (channel_id, channel_name, channel_type, categoryId, category, guild_id, topic, channel_name_length)
-SELECT
-  json_extract(raw_json, '$.id') as channel_id,
-  json_extract(raw_json, '$.name') as channel_name,
-  json_extract(raw_json, '$.type') as channel_type,
-  json_extract(raw_json, '$.categoryId') as categoryId,
-  json_extract(raw_json, '$.category') as category,
-  json_extract(raw_json, '$.guild_id') as guild_id,
-  json_extract(raw_json, '$.topic') as topic,
-  LENGTH(json_extract(raw_json, '$.name')) as channel_name_length
-FROM
-  raw_channels_t;
-""",
-
-"""
-CREATE TABLE IF NOT EXISTS guilds_t (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  guild_id    TEXT,
-  guild_name  TEXT,
-  iconUrl     TEXT
-)
-""",
-
-"""
-INSERT INTO guilds_t (guild_id, guild_name, iconUrl)
-SELECT
-  DISTINCT(json_extract(raw_json, '$.id')) as guild_id,
-  json_extract(raw_json, '$.name') as guild_name,
-  json_extract(raw_json, '$.iconUrl') as iconUrl
-FROM raw_guilds_t;
-""",
-
-"""
 CREATE TABLE IF NOT EXISTS attachments_t (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  attachment_id TEXT,
-  attachment_url TEXT,
-  attachment_filename TEXT,
-  fileSizeBytes INTEGER,
-  message_id TEXT
+  id                    INTEGER PRIMARY KEY,
+  attachment_url        TEXT,
+  attachment_filename   TEXT,
+  fileSizeBytes         INTEGER,
+  message_id            INTEGER
 )
 """,
 
 """
-INSERT INTO attachments_t (attachment_id, attachment_url, attachment_filename, fileSizeBytes, message_id)
+INSERT INTO attachments_t (id, attachment_url, attachment_filename, fileSizeBytes, message_id)
 SELECT
-  DISTINCT(json_extract(raw_json, '$.id')) as attachment_id,
+  DISTINCT(json_extract(raw_json, '$.id')) as id,
   json_extract(raw_json, '$.attachment_url') as attachment_url,
   json_extract(raw_json, '$.fileName') as attachment_filename,
   json_extract(raw_json, '$.fileSizeBytes') as fileSizeBytes,
@@ -177,46 +204,21 @@ FROM raw_attachments_t;
 
 """
 CREATE TABLE IF NOT EXISTS roles_t (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  role_id TEXT,
-  role_name TEXT,
-  color TEXT,
-  position INTEGER
+  id          INTEGER PRIMARY KEY,
+  role_name   TEXT,
+  color       TEXT,
+  position    INTEGER
 )
 """,
 
 """
-INSERT INTO roles_t (role_id, role_name, color, position)
+INSERT INTO roles_t (id, role_name, color, position)
 SELECT
-  DISTINCT(json_extract(raw_json, '$.id')) as role_id,
+  DISTINCT(json_extract(raw_json, '$.id')) as id,
   json_extract(raw_json, '$.role_name') as role_name,
   json_extract(raw_json, '$.color') as color,
   json_extract(raw_json, '$.position') as position
 FROM raw_roles_t;
-""",
-
-"""
-CREATE TABLE IF NOT EXISTS authors_t (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  author_id    TEXT,
-  name         TEXT,
-  nickname     TEXT,
-  color        TEXT,
-  isBot        BOOLEAN,
-  avatarUrl    TEXT
-)
-""",
-
-"""
-INSERT INTO authors_t (author_id, name, nickname, color, isBot, avatarUrl)
-SELECT
-  DISTINCT(json_extract(raw_json, '$.id')) as author_id,
-  json_extract(raw_json, '$.name') as name,
-  json_extract(raw_json, '$.nickname') as nickname,
-  json_extract(raw_json, '$.color') as color,
-  json_extract(raw_json, '$.isBot') as isBot,
-  json_extract(raw_json, '$.avatarUrl') as avatarUrl
-FROM raw_authors_t;
 """
 
 ]
